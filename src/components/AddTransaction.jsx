8ab6f6743c2721fa1f8f8d2b0e9a3cefe14078c5
ashish <128252821+aashishjoshi5@users.noreply.github.com>
2024-08-25 23:41:41 +0530

    third commit

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -1,17 +1,30 @@
-import React,{useState} from 'react'
+import React,{useState,useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 
 function AddTransaction() {
-    const [Text, setText]=useState('');
-    const [Amount, setAmount]=useState(0);
+    const [text, setText]=useState('');
+    const [amount, setAmount]=useState(0);
+
+    const {addTransaction}=useContext(GlobalContext);
+    const onSubmit=e=>{
+      e.preventDefault();
+
+      const newTransaction ={
+        id:  Math.floor(Math.random() * 100000000),
+        text,
+        amount: +amount
+      }
+      addTransaction(newTransaction);
+    }
 
     return (
         <>
          <h3>Add new transaction</h3>
-      <form >
+      <form  onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" value={Text} onChange={(e)=>setText(e.target.value)
+          <input type="text" value={text} onChange={(e)=>setText(e.target.value)
           } placeholder="Enter text..." />
         </div>
         <div className="form-control">
@@ -19,7 +32,7 @@ function AddTransaction() {
             >Amount <br />
             (negative - expense, positive - income)</label
           >
-          <input type="number" value={Amount} onChange={(e)=>setAmount(e.target.value)
+          <input type="number" value={amount} onChange={(e)=>setAmount(e.target.value)
           } placeholder="Enter amount..." />
         </div>
         <button className="btn">Add transaction</button>
